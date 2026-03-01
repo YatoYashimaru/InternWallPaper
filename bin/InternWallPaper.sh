80538fd58c44f91312d0b016ae726f7ec96cfb92
YatoYashimaru <lehoangthien2007@gmail.com>
2026-03-01 19:11:07 +0700

    Add files via upload

---
+++ bin/InternWallPaper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Path to your wallpaper folder - Change this to your actual folder!
+DIR="/usr/local/share"
+
+# Pick a random file and apply it with swww
+PICS=$(find "$DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \))
+
+RANDOM_PIC=$(echo "$PICS" | shuf -n 1)
+
+swww img "$RANDOM_PIC" --transition-type wave --transition-fps 60
+
+case $1 in
+	"change")
+		swww img "$RANDOM_PIC" --transition-type wave --transition-fps 60
+		;;
+	*)
+		echo "Out of expected"
+		;;
+esac
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Move the files
+sudo cp bin/InternWallPaper.sh /usr/local/bin
+sudo cp -r Wallpapers /usr/local/share
+
+# Make the script executable
+sudo chmod +x /usr/local/bin/InternWallPaper
+
+echo "InternWallPaper Installation completed"
+echo "swww must installed to be use"
