c953ab4258401ee3513d1e5d0599ad075860ee9a
YatoYashimaru <lehoangthien2007@gmail.com>
2026-03-23 15:26:22 +0700

    v1.2.1 fixed

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Move the files
-sudo cp bin/InternWallPaper.sh /usr/local/bin
-
 DIS=$(eval echo "~$SUDO_USER")
 cp -r WallpapersAssets "$DIS/"
 
-chown -R $SUDO_USER:$SUDO_USER
-"$DIS/WallpapersAssets"
+sudo cp bin/InternWallPaper.sh /usr/local/bin
 
 # Make the script executable
 sudo chmod +x /usr/local/bin/InternWallPaper.sh
