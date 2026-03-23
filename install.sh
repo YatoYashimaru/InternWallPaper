3ff48fad56be17116b05dfa6935621085972a65e
YatoYashimaru <lehoangthien2007@gmail.com>
2026-03-23 15:19:28 +0700

    v1.2 fixed

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 # Move the files
 sudo cp bin/InternWallPaper.sh /usr/local/bin
-cp -r WallpapersAssets "$HOME/"
+
+DIS=$(eval echo "~$SUDO_USER")
+cp -r WallpapersAssets "$DIS/"
+
+chown -R $SUDO_USER:$SUDO_USER
+"$DIS/WallpapersAssets"
 
 # Make the script executable
 sudo chmod +x /usr/local/bin/InternWallPaper.sh
