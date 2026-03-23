1a002f8559c42bbc63d6ac329cf2f16bc508ce70
YatoYashimaru <lehoangthien2007@gmail.com>
2026-03-23 15:09:23 +0700

    v 1.2

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 # Move the files
 sudo cp bin/InternWallPaper.sh /usr/local/bin
-sudo cp -r WallpapersAssets $HOME
+cp -r WallpapersAssets "$HOME/"
 
 # Make the script executable
 sudo chmod +x /usr/local/bin/InternWallPaper.sh
 
+echo "---------------------------------------"
 echo "InternWallPaper Installation completed"
 echo "swww must installed to be use"
+echo "---------------------------------------"
