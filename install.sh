c47c2420aa716c62bd73032a4598407fd63719da
YatoYashimaru <lehoangthien2007@gmail.com>
2026-07-12 11:21:09 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Move the files
 DIS=$(eval echo "~$SUDO_USER")
 cp -r WallpapersAssets "$DIS/"
-chown -R $SUDO_USER:$SUDO_USER "$DIS/"
+sudo chown -R $SUDO_USER:$SUDO_USER "$DIS/WallpapersAssets"
 
 sudo cp bin/InternWallPaper.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/InternWallPaper.sh
@@ -14,7 +14,8 @@ echo "awww must installed to be use"
 echo "---------------------------------------"
 echo "Installing awww(swww)"
 
-sudo pacman -S awww
+sudo pacman -Rns --noconfirm  awww 2>/dev/null || true
+sudo pacman -S --noconfirm awww 
 
 echo "---------------------------------------"
 echo "completed"
